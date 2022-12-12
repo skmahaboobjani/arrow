d0ae195d7adebfcc551f829735e2332738014ab4
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2022-12-12 08:41:35 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,20 @@
+// Arrow functions were introduced in ES6.
+
+// Arrow functions allow us to write shorter function syntax
+
+// this is new way to define the functions in javascript
+
+
+let m=(a,b)=>
+{
+    return a+b
+}
+console.log(m(1,2))
+
+//the function which cannot have no name are called ananymous functions
+
+let hello=function()
+{
+    console.log("iam anonymous")
+}
+hello()
